b50e37bcf39c9a9dd6b57527ae76986295fdbca8
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-09-09 15:16:50 +0530

    Update 5.cpp

--- Week_5/5.cpp
+++ Week_5/5.cpp
@@ -1,3 +1,32 @@
-//
-// Created by Lenovo on 9/9/2025.
-//
+#include <bits/stdc++.h>
+using namespace std;
+class stUSINGq {
+    queue<int> q;
+    queue<int> temp;
+public:
+    void push(int x) {
+        while (!q.empty()) {
+            temp.push(q.front()); q.pop();
+        }
+        q.push(x);
+        while (!temp.empty()) {
+            q.push(temp.front()); temp.pop();
+        }
+    }
+    void pop() {
+        q.pop();
+    }
+    int size() {
+        return q.size();
+    }
+    int top() {
+        return q.front();
+    }
+};
+int main() {
+    stUSINGq st;
+    st.push(10);
+    st.push(20);
+    cout<<st.top();
+    return 0;
+}
