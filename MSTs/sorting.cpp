0a81dd1a6d25c7e8f18c683835be2d84afc2cc9b
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-09-20 00:21:37 +0530

    add MST practice directory

---
+++ MSTs/sorting.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void selectionSort(int *arr, int n) {
+    int min;
+    for (int i = 0; i < n-1; i++) {
+        min = i;
+        for (int j = i; j < n; j++) {
+            if (arr[min] > arr [j]) {
+                min = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        bool isSwapped = 0;
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                isSwapped = 1;
+            }
+        }
+        if (!isSwapped) {
+            break;
+        }
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    for (int i = 0; i < n ; i++) {
+        int j = i;
+        while (j > 0 && arr[j-1] > arr[j]) {
+            swap(arr[j-1], arr[j]);
+            j--;
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main() {
+    int arr[] = {29, 10, 14, 37, 13};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printArray(arr, size);
+    // selectionSort(arr, size);
+    // bubbleSort(arr, size);
+    insertionSort(arr, size);
+    printArray(arr, size);
+
+    return 0;
+}
