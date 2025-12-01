181ff09d8de75601087d1095331e34eefcef1047
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-12-01 15:03:55 +0530

    Add bubble sort implementation in 1c.cpp
    Implemented bubble sort algorithm to sort an array.

---
+++ Week_10/1c.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {8,5,1,4,2};
+    int n = 5;
+
+    // bubble sorting
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+
+    cout<<"Bubble Sorted: ";
+    for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+}
